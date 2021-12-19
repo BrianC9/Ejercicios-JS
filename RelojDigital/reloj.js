2ff57afbd18f8c46d137b931b0982ea92b6f9ea5
Bryan Octavio <100432689@alumnos.uc3m.es>
2021-12-19 21:49:04 +0100

    Reloj con JS
    setInterval

---
+++ RelojDigital/reloj.js
@@ -0,0 +1,19 @@
+// // Creamos una funncion que se ejecute cada segundo, para que vaya cambiando el reloj
+setInterval(() => {
+    let reloj = document.getElementsByClassName("reloj")[0];
+    let fechaActual = new Date();
+    let hora = fechaActual.getHours();
+    if (hora < 10) {
+        hora = `0${hora}`;
+    }
+    let minutos = fechaActual.getMinutes();
+    if (minutos < 10) {
+        minutos = `0${minutos}`;
+    }
+    let segundos = fechaActual.getSeconds();
+    if (segundos < 10) {
+        segundos = `0${segundos}`;
+    }
+
+    reloj.innerHTML = `${hora}:${minutos}:${segundos}`;
+}, 1000);
