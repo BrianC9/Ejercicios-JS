4437bef2b7a5812d320c108dd7ee6cc25b3516ed
Bryan Octavio <100432689@alumnos.uc3m.es>
2021-12-26 19:53:10 +0100

    Layout de calculadora / Falta JS

---
+++ Calculadora/app.js
@@ -0,0 +1,10 @@
+//Seleccionamos todos los botones y campos de HTML que vamos a necesitar para poder operar
+
+const btnNumeros = document.querySelectorAll("[data-number");
+const btnOperadores = document.querySelectorAll("[data-operation");
+const btnBorarrTodo
+const btnIgual = document.querySelector("[data-equals]");
+const btnBorradoSimple = document.querySelector("[data-delete]");
+const btnBorrarTodo = document.querySelector("[data-allclear]");
+const textoOperandoAnterior = document.querySelector("[data-operando-anterior]");
+const textoOperandoActual = document.querySelector("[data-operando-actual]");
